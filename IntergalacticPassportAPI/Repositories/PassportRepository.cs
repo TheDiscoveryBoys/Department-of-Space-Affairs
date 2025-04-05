a3b01626b0d9564b5485fbca1f32ed0dc515e710
GregoryMaselle-W <Gregory.Maselle@bbd.co.za>
2025-04-06 00:42:25 +0200

    TODO: Rename entity fields to work with queries. Mismatches cause NULLs to be returned.

--- IntergalacticPassportAPI/Repositories/PassportRepository.cs
+++ IntergalacticPassportAPI/Repositories/PassportRepository.cs
@@ -22,7 +22,7 @@ namespace IntergalacticPassportAPI.Data
         public async Task<IEnumerable<Passport>> GetAllAsync()
         {
             using var connection = CreateConnection();
-            return await connection.QueryAsync<Passport>("SELECT * FROM passport ORDER BY id;");
+            return await connection.QueryAsync<Passport>("SELECT id, user_id AS UserId, status_id AS StatusId,  submitted_at AS SubmittedAt,  processed_at AS ProcessedAt,  officer_id AS OfficerId FROM passport ORDER BY id;");
         }
 
         public async Task<Passport?> GetByIdAsync(int id)
@@ -37,8 +37,8 @@ namespace IntergalacticPassportAPI.Data
             try
             {
                 using var connection = CreateConnection();
-                var sql = @"INSERT INTO passports (name, species, planet, issued_date)
-                        VALUES (@Name, @Species, @Planet, @IssuedDate)
+                var sql = @"INSERT INTO passports (user_id, status_id, submitted_at, processed_at, officer_id)
+                        VALUES (@UserId, @StatusId, @SubmittedAt, @ProcessedAt)
                         RETURNING id;";
                 return await connection.ExecuteScalarAsync<int>(sql, passport);
             }
