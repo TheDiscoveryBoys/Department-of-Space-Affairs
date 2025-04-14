0e783afb7463676ba8da2034a659bc050b335132
Liam Barkley <Liam.Barkley@bbd.co.za>
2025-04-14 08:41:47 +0200

    minor fixes

--- DOSA-Client/lib/StarWarsClient.cs
+++ DOSA-Client/lib/StarWarsClient.cs
@@ -1,17 +1,7 @@
-﻿using System.ComponentModel;
-using System.Net;
-using System.Windows.Documents;
-using System.Windows.Media.Animation;
-using DOSA_Client.Models;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Json;
 using DOSA_Client.lib.Constants;
-using System.Configuration;
-using static DOSA_Client.ViewModels.UploadPassportDocumentsViewModel;
-using System.IO;
-using System.Net.Http.Headers;
-using System.Reflection.Metadata;
-using System.Windows;
+using DOSA_Client.Models;
 
 public static class StarWarsClient
 {
