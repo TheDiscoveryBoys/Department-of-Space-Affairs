9f1fc101cbec65640edc287ed338821a1f746d02
Shashin Gounden <shashin.gounden@bbd.co.za>
2025-04-16 22:14:29 +0200

    migration fix

--- migrations/V202504162124__InsertTravelReasons.sql
+++ migrations/V202504162124__InsertTravelReasons.sql
@@ -1,31 +1,31 @@
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Tourist & Visitor Visas') THEN
-        INSERT INTO travel_reasons (reason) VALUES ('Tourist & Visitor Visas');
+    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Tourist & Visitor') THEN
+        INSERT INTO travel_reasons (reason) VALUES ('Tourist & Visitor');
     END IF;
 
-    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Student Visas') THEN
-        INSERT INTO travel_reasons (reason) VALUES ('Student Visas');
+    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Student') THEN
+        INSERT INTO travel_reasons (reason) VALUES ('Student');
     END IF;
 
-    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Work Visas') THEN
-        INSERT INTO travel_reasons (reason) VALUES ('Work Visas');
+    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Work') THEN
+        INSERT INTO travel_reasons (reason) VALUES ('Work');
     END IF;
 
-    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Immigration / Settlement Visas') THEN
-        INSERT INTO travel_reasons (reason) VALUES ('Immigration / Settlement Visas');
+    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Immigration / Settlement') THEN
+        INSERT INTO travel_reasons (reason) VALUES ('Immigration / Settlement');
     END IF;
 
-    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Business & Investment Visas') THEN
-        INSERT INTO travel_reasons (reason) VALUES ('Business & Investment Visas');
+    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Business & Investment') THEN
+        INSERT INTO travel_reasons (reason) VALUES ('Business & Investment');
     END IF;
 
-    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Transit / Crew Visas') THEN
-        INSERT INTO travel_reasons (reason) VALUES ('Transit / Crew Visas');
+    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Transit / Crew') THEN
+        INSERT INTO travel_reasons (reason) VALUES ('Transit / Crew');
     END IF;
 
-    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Special Purpose / Other Visas') THEN
-        INSERT INTO travel_reasons (reason) VALUES ('Special Purpose / Other Visas');
+    IF NOT EXISTS (SELECT 1 FROM travel_reasons WHERE reason = 'Special Purpose / Other') THEN
+        INSERT INTO travel_reasons (reason) VALUES ('Special Purpose / Other');
     END IF;
 END
 $$;
