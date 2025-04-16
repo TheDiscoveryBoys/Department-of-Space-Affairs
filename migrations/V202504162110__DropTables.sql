b727ab68e6ee7f81ba6b026d83c79c524768bdbb
Shashin Gounden <shashin.gounden@bbd.co.za>
2025-04-16 22:05:22 +0200

    migration fix

--- migrations/V202504162110__DropTables.sql
+++ migrations/V202504162110__DropTables.sql
@@ -1,2 +1,9 @@
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
+DROP TABLE IF EXISTS
+  "passport_application_documents",
+  "user_roles",
+  "passport_applications",
+  "visa_applications",
+  "roles",
+  "statuses",
+  "users"
+CASCADE;
